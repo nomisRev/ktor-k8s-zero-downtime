598c1e41ec0a3cb65c1fa5421fc3c77068bba849
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-02-15 16:26:58 +0100

    Step 1 - embeddedServer

--- ktor-native-server/build.gradle.kts
+++ ktor-native-server/build.gradle.kts
@@ -7,7 +7,7 @@
 }
 
 group = "com.fortysevendegrees"
-version = "0.2"
+version = "0.1"
 
 repositories {
   mavenCentral()
--- ktor-native-server/src/commonMain/kotlin/com/fortysevendegrees/main.kt
+++ ktor-native-server/src/commonMain/kotlin/com/fortysevendegrees/main.kt
@@ -1,37 +1,19 @@
 package com.fortysevendegrees
 
-import arrow.continuations.SuspendApp
-import arrow.continuations.ktor.server
-import arrow.core.raise.either
-import arrow.fx.coroutines.resourceScope
-import com.fortysevendegrees.env.Env
-import com.fortysevendegrees.env.env
-import com.fortysevendegrees.routes.ping
-import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.application.install
 import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.server.plugins.cors.maxAgeDuration
-import io.ktor.server.plugins.cors.routing.CORS
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 import kotlin.random.Random
-import kotlin.time.Duration.Companion.days
-import kotlin.time.Duration.Companion.seconds
-import kotlinx.coroutines.awaitCancellation
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.sync.Mutex
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
-
-//import kotlinx.uuid.UUID
-//import kotlinx.uuid.generateUUID
-//import kotlinx.uuid.randomUUID
 
 @Serializable
 data class Payload(
@@ -43,40 +25,23 @@ data class Payload(
   }
 }
 
-fun main() = SuspendApp {
-//  either {
-    resourceScope {
-//      val env = env()
-//    @Suppress("UnusedPrivateMember")
-//    val database = postgres(env.postgres)
-      server(
-        CIO,
-        port = 8080,
-        host = "0.0.0.0",
-        preWait = 0.seconds
-      ) {
-//        install(CORS) {
-//          allowHeader(HttpHeaders.Authorization)
-//          allowHeader(HttpHeaders.ContentType)
-//          allowNonSimpleContentTypes = true
-//          maxAgeDuration = 3.days
-//        }
-//        install(ContentNegotiation) {
-//          json(
-//            Json {
-//              isLenient = true
-//              ignoreUnknownKeys = true
-//            }
-//          )
-//        }
-//        install(CallLogging)
-        routing {
-          ping()
-          get("/readiness") { call.respondText("OK") }
-//          get("/test1") { call.respond(Payload()) }
-        }
-      }
-      awaitCancellation()
+fun main() {
+  embeddedServer(CIO, port = 8080) {
+    setup()
+    routing {
+      get("/readiness") { call.respondText("OK") }
+      get("/test") { call.respond(Payload()) }
     }
-//  }.onLeft { println(it.message) }
+  }.start(wait = true)
+}
+
+fun Application.setup() {
+  install(ContentNegotiation) {
+    json(
+      Json {
+        isLenient = true
+        ignoreUnknownKeys = true
+      }
+    )
+  }
 }
