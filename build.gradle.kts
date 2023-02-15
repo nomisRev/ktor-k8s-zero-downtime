418a07b2da8d628cb120c92ccc7326a0e3fef0c8
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-02-15 12:22:26 +0100

    WIP

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform) apply false
+}
+
+group = "io.github.nomisrev"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+rootProject.name = "webinar-16-02-2023"
+
+include(":ktor-native-server")
+
